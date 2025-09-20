7224d2e9aff93f5210b17f39c20086485a1ca2cb
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-20 10:58:23 +0500

    1. Забытая точка с запятой

---
+++ src/main/java/ru/job4j/basics/scheme/HelloWorld.java
@@ -0,0 +1,10 @@
+package ru.job4j.basics.scheme;
+
+/*
+  Каждый оператор в Java должен заканчиваться точкой с запятой. Исправьте ошибку в приведенном коде.
+*/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Job4j!");
+    }
+}
---
+++ src/test/java/ru/job4j/basics/scheme/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.basics.scheme;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class HelloWorldTest {
+    @Test
+    public void checkOutHelloJob4j() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        HelloWorld.main(null);
+        assertThat(out.toString(), is("Hello, Job4j!" + System.lineSeparator()));
+    }
+}
