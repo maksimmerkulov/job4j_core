db0f7c7a21b57f0688fb1aae595f98fc97ef1a5c
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-26 09:13:37 +0500

    14. Вывод на консоль надписи "H-e-l-l-o-,-f-r-i-e-n-d!"

---
+++ src/main/java/ru/job4j/basics/console/Task14.java
@@ -0,0 +1,14 @@
+package ru.job4j.basics.console;
+
+/*
+  Напишите класс, который выводит на экран надпись
+
+  H-e-l-l-o-,-f-r-i-e-n-d!
+
+  без перевода на новую строку.
+*/
+public class Task14 {
+    public static void main(String[] args) {
+        System.out.print("H-e-l-l-o-,-f-r-i-e-n-d!");
+    }
+}
