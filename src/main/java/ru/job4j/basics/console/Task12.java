922675aa7f58dfea99461a20f7b3afceebb31569
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-26 09:08:55 +0500

    12. Вывод Ping Pong на консоль

---
+++ src/main/java/ru/job4j/basics/console/Task12.java
@@ -0,0 +1,17 @@
+package ru.job4j.basics.console;
+
+/*
+  Допишите метод main, что программа выводила на консоль две строчки
+
+  Ping
+
+  Pong
+
+  Программа должна печатать каждое слово на новой строке.
+*/
+public class Task12 {
+    public static void main(String[] args) {
+        System.out.println("Ping");
+        System.out.println("Pong");
+    }
+}
