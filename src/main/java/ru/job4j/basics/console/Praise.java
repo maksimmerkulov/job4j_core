edd138a73234198362109a88c6d070b08eaeee51
maksimmerkulov <merkulov.mv@outlook.com>
2025-10-12 16:52:04 +0500

    1.1.1. Вывод информации на консоль: 10. Похвали себя за проделанную работу

---
+++ src/main/java/ru/job4j/basics/console/Praise.java
@@ -0,0 +1,8 @@
+package ru.job4j.basics.console;
+
+// Напишите программу, которая выводит на консоль строчку "Сегодня я молодец!"
+public class Praise {
+    public static void main(String[] args) {
+        System.out.println("Сегодня я молодец!");
+    }
+}
