01d1eee86b20cada48e76c101759be4af9fde5bd
maksimmerkulov <merkulov.mv@outlook.com>
2025-10-12 16:50:12 +0500

    1.1.1. Вывод информации на консоль: 9. Вывод информации на экран

---
+++ src/main/java/ru/job4j/basics/console/HelloWorld.java
@@ -0,0 +1,15 @@
+package ru.job4j.basics.console;
+
+/*
+  Чтобы вывести текст на экран в Java используется оператор.
+
+  System.out.println("Hello, Job4j!");
+
+  Допишите программу приведенную ниже. Программа должна выводить на консоль
+  строку "Hello, Job4j!"
+*/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Job4j!");
+    }
+}
