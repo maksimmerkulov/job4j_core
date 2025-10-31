d8292f124ead881a08e259b3b965e24f57043b03
maksimmerkulov <merkulov.mv@outlook.com>
2025-10-31 21:12:55 +0500

    1.1.4. Оператор ветвления: 17. Вывести знак числа

---
+++ src/main/java/ru/job4j/basics/branching/Task17.java
@@ -0,0 +1,23 @@
+package ru.job4j.basics.branching;
+
+/*
+  Ниже представлен метод signCheck(int number), который принимает целое число.
+
+  Задание: Написать код, который выводит в консоль одно из слов: "Положительное",
+  "Ноль" или "Отрицательное", в зависимости от значения входящего числа.
+*/
+public class Task17 {
+    public static void signCheck(int number) {
+        if (number > 0) {
+            System.out.println("Положительное");
+        } else if (number < 0) {
+            System.out.println("Отрицательное");
+        } else {
+            System.out.println("Ноль");
+        }
+    }
+
+    public static void main(String[] args) {
+        signCheck(0);
+    }
+}
