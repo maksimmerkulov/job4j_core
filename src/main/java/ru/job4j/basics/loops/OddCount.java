4cc658f73a79e8a1bdf6d1ff45d3ca844b7ac4b1
maksimmerkulov <merkulov.mv@outlook.com>
2026-01-13 23:48:13 +0500

    1.1.5. Циклы: 8. Вывести делители числа

---
+++ src/main/java/ru/job4j/basics/loops/OddCount.java
@@ -0,0 +1,22 @@
+package ru.job4j.basics.loops;
+
+/*
+  Даны два целых положительных числа a и b (b > a). Найти количество нечетных
+  чисел в диапазоне [a, b]. Нечетным считается число, у которого остаток от
+  деления на 2 равен 1.
+*/
+public class OddCount {
+    public static int count(int a, int b) {
+        int result = 0;
+        for (int i = a; i <= b; i++) {
+            if (i % 2 == 1) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(count(1, 2));
+    }
+}
