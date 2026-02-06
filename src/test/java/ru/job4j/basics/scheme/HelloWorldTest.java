29756bf78511c2ba31a1fdef4de37993639b0d7b
maksimmerkulov <merkulov.mv@outlook.com>
2026-02-06 10:40:00 +0500

    1.1.0. Базовая схема кода: 8. Забытая скобка

--- src/test/java/ru/job4j/basics/scheme/HelloWorldTest.java
+++ src/test/java/ru/job4j/basics/scheme/HelloWorldTest.java
@@ -18,7 +18,7 @@ import static org.junit.Assert.assertThat;
  * с учетом системного переноса строки.</p>
  *
  * @author Maksim Merkulov
- * @version 1.6
+ * @version 1.7
  */
 public class HelloWorldTest {
 
