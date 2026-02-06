e115e2d8e9b7e70ea4bc89ed30653968766a6cf8
maksimmerkulov <merkulov.mv@outlook.com>
2026-02-06 10:05:00 +0500

    1.1.0. Базовая схема кода: 1. Забытая точка с запятой

---
+++ src/main/java/ru/job4j/basics/scheme/HelloWorld.java
@@ -0,0 +1,27 @@
+package ru.job4j.basics.scheme;
+
+/**
+ * Класс {@code HelloWorld} выводит приветственное сообщение на консоль.
+ *
+ * <p>Содержит единственный метод {@code main}, который демонстрирует
+ * базовый вывод текста на консоль.</p>
+ *
+ * <p>Примеры использования: запуск программы выводит строку "Hello, Job4j!"
+ * на стандартный вывод.</p>
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class HelloWorld {
+
+    /**
+     * Точка входа в программу.
+     *
+     * <p>Выводит на консоль приветственное сообщение "Hello, Job4j!".</p>
+     *
+     * @param args аргументы командной строки (не используются)
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, Job4j!");
+    }
+}
---
+++ src/test/java/ru/job4j/basics/scheme/HelloWorldTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.basics.scheme;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Модульные тесты класса {@link HelloWorld}.
+ *
+ * <p>Проверяется корректность вывода текста на консоль
+ * методом {@link HelloWorld#main(String[])}.</p>
+ *
+ * <p>Результаты сравниваются с ожидаемой строкой
+ * с учетом системного переноса строки.</p>
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class HelloWorldTest {
+
+    /**
+     * Проверяет метод {@link HelloWorld#main(String[])}
+     * при выводе строки "Hello, Job4j!" на консоль.
+     *
+     * <p>Ожидаемый результат: корректная строка с переносом.</p>
+     */
+    @Test
+    public void checkOutHelloJob4j() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        HelloWorld.main(null);
+        assertThat(out.toString(), is("Hello, Job4j!"
+                + System.lineSeparator()));
+    }
+}
