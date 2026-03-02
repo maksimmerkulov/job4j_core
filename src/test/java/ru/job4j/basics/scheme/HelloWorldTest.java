0503447b9a92f96e45cbd97b7038651b2a6be69d
maksimmerkulov <merkulov.mv@outlook.com>
2026-03-02 12:00:00 +0500

    1.1.0. Базовая схема кода: 8. Забытая скобка

--- src/test/java/ru/job4j/basics/scheme/HelloWorldTest.java
+++ src/test/java/ru/job4j/basics/scheme/HelloWorldTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Tests for the {@link HelloWorld} class.
  *
  * @author Maksim Merkulov
- * @version 1.6
+ * @version 1.7
  */
 class HelloWorldTest {
 
