bc9d1420d10d2009247653759c40bab26450c177
maksimmerkulov <merkulov.mv@outlook.com>
2026-03-03 10:15:00 +0500

    1.1.1. Вывод информации на консоль: 9. Вывод информации на экран

---
+++ src/main/java/ru/job4j/basics/console/HelloWorld.java
@@ -0,0 +1,24 @@
+package ru.job4j.basics.console;
+
+/**
+ * Prints a greeting message to the console.
+ *
+ * <p>Example output:
+ * <pre>{@code
+ * Hello, Job4j!
+ * }</pre>
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class HelloWorld {
+
+    /**
+     * Entry point of the program.
+     *
+     * @param args command-line arguments; not used
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, Job4j!");
+    }
+}
---
+++ src/test/java/ru/job4j/basics/console/HelloWorldTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.basics.console;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for the {@link HelloWorld} class.
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+class HelloWorldTest {
+
+    /** Verifies the console output of the {@code main} method. */
+    @Test
+    void whenMainThenPrintHelloJob4j() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream stdout = System.out;
+        System.setOut(new PrintStream(out));
+        HelloWorld.main(null);
+        System.setOut(stdout);
+        assertThat(out.toString())
+                .isEqualTo("Hello, Job4j!" + System.lineSeparator());
+    }
+}
